33305894556f9bc3a9621a94304a2ad85d1d22f2
olivercode <jianheng11@sina.com>
2015-08-14 19:50:34 +0800

    加域脚本

---
+++ jiondomain.ps1
@@ -0,0 +1,10 @@
+﻿
+##计算机加入域
+
+$h=hostname
+
+netdom join $h  /domain:XDFDC.CN /userd:wangjianheng-admin /passwordd:"xxxxx"
+
+net localgroup administrators xdfdc\sqlserverdba /add
+
+net localgroup administrators xdfdc\implement-baoming /add
